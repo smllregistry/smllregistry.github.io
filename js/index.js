37d8c103efcd934269eeec071633a45b14dcb385
hexaredecimal <mfanakagama@gmail.com>
2024-05-05 12:55:36 +0200

    Toast is not working

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ function onLoad() {
     for (data of json) {
       make_component(data, search)
     }
-    document.body.innerHTML = `<div id="snackbar">Some text some message..</div>`
+    document.body.innerHTML = `<div id="snackbar">To Be Replaced</div>`
   }).catch(error => {
     console.error('Error loading file:', error);
   });
